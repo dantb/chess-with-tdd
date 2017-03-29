7bcd5ca6372a3b5a184be5eb2d0e7eef3bafeb5a
dantb <dantb@hotmail.co.uk>
2017-03-29 20:19:24 +0100

    Restructuring of folders.

---
+++ ChessWithTDD/Tests/Pieces/BlackPawnTests.cs
@@ -0,0 +1,188 @@
+﻿using NUnit.Framework;
+using static ChessWithTDD.Tests.CommonTestMethods;
+
+namespace ChessWithTDD.Tests
+{
+    [TestFixture]
+    public class BlackPawnTests
+    {
+        #region Normal move
+
+        [TestCase(5, 2, 4, 2)]
+        [TestCase(3, 3, 2, 3)]
+        [Test]
+        public void BlackPawnCanMoveOneVerticalPositionDownTheBoard(int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            IPiece pawn = new BlackPawn();
+            ISquare fromSquare = MockSquareWithPiece(rowFrom, colFrom, pawn);
+            ISquare toSquare = MockSquareWithoutPiece(rowTo, colTo);
+
+            bool canPawnMove = pawn.CanMove(fromSquare, toSquare);
+
+            Assert.True(canPawnMove);
+        }
+
+        [TestCase(5, 2, 4, 2)]
+        [TestCase(3, 3, 2, 3)]
+        [Test]
+        public void BlackPawnCannotMoveOneVerticalPositionDownTheBoardIfWhitePieceThere(int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            IPiece pawn = new BlackPawn();
+            ISquare fromSquare = MockSquareWithPiece(rowFrom, colFrom, pawn);
+            IPiece whitePiece = MockPieceWithColour(Colour.White);
+            ISquare toSquare = MockSquareWithPiece(rowTo, colTo, whitePiece);
+
+            bool canPawnMove = pawn.CanMove(fromSquare, toSquare);
+
+            Assert.False(canPawnMove);
+        }
+
+        #endregion Normal move
+
+
+        #region Taking a piece
+
+        [TestCase(6, 6, 5, 7)]
+        [TestCase(6, 6, 5, 5)]
+        [Test]
+        public void BlackPawnCanMoveDiagonallyDownTheBoardIfWhitePieceThere(int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            IPiece pawn = new BlackPawn();
+            ISquare fromSquare = MockSquareWithPiece(rowFrom, colFrom, pawn);
+            IPiece whitePiece = MockPieceWithColour(Colour.White);
+            ISquare toSquare = MockSquareWithPiece(rowTo, colTo, whitePiece);
+
+            bool canPawnMove = pawn.CanMove(fromSquare, toSquare);
+
+            Assert.True(canPawnMove);
+        }
+
+        [TestCase(6, 6, 5, 7)]
+        [TestCase(6, 6, 5, 5)]
+        [Test]
+        public void BlackPawnCannotMoveDiagonallyBelowIfNoPieceThere(int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            IPiece pawn = new BlackPawn();
+            ISquare fromSquare = MockSquareWithPiece(rowFrom, colFrom, pawn);
+            ISquare toSquare = MockSquareWithoutPiece(rowTo, colTo);
+
+            bool canPawnMove = pawn.CanMove(fromSquare, toSquare);
+
+            Assert.False(canPawnMove);
+        }
+
+        #endregion Taking a piece
+
+        #region Taking a white pawn using en passant capture
+
+        [TestCase(6, 6, 5, 7)]
+        [TestCase(6, 6, 5, 5)]
+        public void BlackPawnCanMoveDiagonallyDownwardsToEmptySquareIfToSquareHasEnPassantMark(int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            IPiece pawn = new BlackPawn();
+            ISquare fromSquare = MockSquareWithPiece(rowFrom, colFrom, pawn);
+            //to square contains no piece but is marked with an EnPassantMark
+            ISquare toSquare = MockSquareWithHasEnPassantMark(rowTo, colTo, true);
+
+            bool canPawnMove = pawn.CanMove(fromSquare, toSquare);
+
+            Assert.True(canPawnMove);
+        }
+
+        [TestCase(6, 6, 5, 7)]
+        [TestCase(6, 6, 5, 5)]
+        public void BlackPawnCannotMoveDiagonallyDownwardsToEmptySquareIfToSquareHasNoEnPassantMark(int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            IPiece pawn = new BlackPawn();
+            ISquare fromSquare = MockSquareWithPiece(rowFrom, colFrom, pawn);
+            ISquare toSquare = MockSquareWithHasEnPassantMark(rowTo, colTo, false);
+
+            bool canPawnMove = pawn.CanMove(fromSquare, toSquare);
+
+            Assert.False(canPawnMove);
+        }
+
+        #endregion Taking a white pawn using en passant capture
+
+        #region Double move for first turn
+
+        [TestCase(4, 4, 2, 4)]
+        [TestCase(6, 1, 4, 1)]
+        [Test]
+        public void BlackPawnCanMoveTwoVerticalSpacesDownTheBoardIfItHasNotMovedBefore(int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            IPawn pawn = new BlackPawn();
+            pawn.HasMoved = false;
+            ISquare fromSquare = MockSquareWithPiece(rowFrom, colFrom, pawn);
+            ISquare toSquare = MockSquareWithoutPiece(rowTo, colTo);
+
+            bool canPawnMove = pawn.CanMove(fromSquare, toSquare);
+
+            Assert.True(canPawnMove);
+        }
+
+        [TestCase(4, 4, 2, 4)]
+        [TestCase(6, 1, 4, 1)]
+        [Test]
+        public void BlackPawnCannotMoveTwoVerticalSpacesDownTheBoardIfItHasMovedBefore(int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            IPawn pawn = new BlackPawn();
+            pawn.HasMoved = true;
+            ISquare fromSquare = MockSquareWithPiece(rowFrom, colFrom, pawn);
+            ISquare toSquare = MockSquareWithoutPiece(rowTo, colTo);
+
+            bool canPawnMove = pawn.CanMove(fromSquare, toSquare);
+
+            Assert.False(canPawnMove);
+        }
+
+        [TestCase(4, 4, 2, 4)]
+        [TestCase(6, 1, 4, 1)]
+        [Test]
+        public void BlackPawnCannotMoveTwoVerticalSpacesDownTheBoardIfItHasNotMovedButToSquareContainsAPiece(int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            IPawn pawn = new BlackPawn();
+            pawn.HasMoved = false;
+            ISquare fromSquare = MockSquareWithPiece(rowFrom, colFrom, pawn);
+            ISquare toSquare = MockSquareWithPiece(rowTo, colTo);
+
+            bool canPawnMove = pawn.CanMove(fromSquare, toSquare);
+
+            Assert.False(canPawnMove);
+        }
+
+        #endregion Double move for first turn
+
+
+        #region Other invalid moves
+
+        [TestCase(0, 1, 1, 1)]
+        [Test]
+        public void BlackPawnCannotMoveOneVerticalPositionUpTheBoard(int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            IPiece pawn = new BlackPawn();
+            ISquare fromSquare = MockSquareWithPiece(rowFrom, colFrom, pawn);
+            ISquare toSquare = MockSquareWithPiece(rowTo, colTo);
+
+            bool canPawnMove = pawn.CanMove(fromSquare, toSquare);
+
+            Assert.False(canPawnMove);
+        }
+
+        [TestCase(5, 2, 5, 3)]
+        [TestCase(3, 3, 3, 2)]
+        [Test]
+        public void BlackPawnCannotMoveOneHorizontalPositionAcrossTheBoard(int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            IPiece pawn = new BlackPawn();
+            ISquare fromSquare = MockSquareWithPiece(rowFrom, colFrom, pawn);
+            ISquare toSquare = MockSquareWithoutPiece(rowTo, colTo);
+
+            bool canPawnMove = pawn.CanMove(fromSquare, toSquare);
+
+            Assert.False(canPawnMove);
+        }
+
+        #endregion Other invalid moves
+    }
+}
---
+++ ChessWithTDD/Tests/Pieces/WhitePawnTests.cs
@@ -0,0 +1,189 @@
+﻿using NUnit.Framework;
+using static ChessWithTDD.Tests.CommonTestMethods;
+
+namespace ChessWithTDD.Tests
+{
+    [TestFixture]
+    public class WhitePawnTests
+    {
+        #region Normal move
+
+        [TestCase(5, 2, 6, 2)]
+        [TestCase(3, 3, 4, 3)]
+        [Test]
+        public void WhitePawnCanMoveOneVerticalPositionUpTheBoard(int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            IPiece pawn = new WhitePawn();
+            ISquare fromSquare = MockSquareWithPiece(rowFrom, colFrom, pawn);
+            ISquare toSquare = MockSquareWithoutPiece(rowTo, colTo);
+
+            bool canPawnMove = pawn.CanMove(fromSquare, toSquare);
+
+            Assert.True(canPawnMove);
+        }
+
+        [TestCase(5, 2, 6, 2)]
+        [TestCase(3, 3, 4, 3)]
+        [Test]
+        public void WhitePawnCannotMoveOneVerticalPositionUpTheBoardIfBlackPieceThere(int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            IPiece pawn = new WhitePawn();
+            ISquare fromSquare = MockSquareWithPiece(rowFrom, colFrom, pawn);
+            IPiece blackPiece = MockPieceWithColour(Colour.Black);
+            ISquare toSquare = MockSquareWithPiece(rowTo, colTo, blackPiece);
+
+            bool canPawnMove = pawn.CanMove(fromSquare, toSquare);
+
+            Assert.False(canPawnMove);
+        }
+
+        #endregion Normal move
+
+
+        #region Taking a piece
+
+        [TestCase(4, 4, 5, 5)]
+        [TestCase(4, 4, 5, 3)]
+        [Test]
+        public void WhitePawnCanMoveDiagonallyUpTheBoardIfBlackPieceThere(int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            IPiece pawn = new WhitePawn();
+            ISquare fromSquare = MockSquareWithPiece(rowFrom, colFrom, pawn);
+            IPiece blackPiece = MockPieceWithColour(Colour.Black);
+            ISquare toSquare = MockSquareWithPiece(rowTo, colTo, blackPiece);
+
+            bool canPawnMove = pawn.CanMove(fromSquare, toSquare);
+
+            Assert.True(canPawnMove);
+        }
+
+        [TestCase(4, 4, 5, 5)]
+        [TestCase(4, 4, 5, 3)]
+        [Test]
+        public void WhitePawnCannotMoveDiagonallyUpTheBoardIfNoPieceThere(int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            IPiece pawn = new WhitePawn();
+            ISquare fromSquare = MockSquareWithPiece(rowFrom, colFrom, pawn);
+            ISquare toSquare = MockSquareWithoutPiece(rowTo, colTo);
+
+            bool canPawnMove = pawn.CanMove(fromSquare, toSquare);
+
+            Assert.False(canPawnMove);
+        }
+
+        #endregion Taking a piece
+
+        #region Taking a white pawn using en passant capture
+
+        [TestCase(4, 4, 5, 5)]
+        [TestCase(4, 4, 5, 3)]
+        public void WhitePawnCanMoveDiagonallyUpwardsToEmptySquareIfToSquareHasEnPassantMark(int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            IPiece pawn = new WhitePawn();
+            ISquare fromSquare = MockSquareWithPiece(rowFrom, colFrom, pawn);
+            //to square contains no piece but is marked with an EnPassantMark
+            ISquare toSquare = MockSquareWithHasEnPassantMark(rowTo, colTo, true);
+
+            bool canPawnMove = pawn.CanMove(fromSquare, toSquare);
+
+            Assert.True(canPawnMove);
+        }
+
+        [TestCase(4, 4, 5, 5)]
+        [TestCase(4, 4, 5, 3)]
+        public void WhitePawnCannotMoveDiagonallyUpwardsToEmptySquareIfToSquareHasNoEnPassantMark(int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            IPiece pawn = new WhitePawn();
+            ISquare fromSquare = MockSquareWithPiece(rowFrom, colFrom, pawn);
+            ISquare toSquare = MockSquareWithHasEnPassantMark(rowTo, colTo, false);
+
+            bool canPawnMove = pawn.CanMove(fromSquare, toSquare);
+
+            Assert.False(canPawnMove);
+        }
+
+        #endregion Taking a white pawn using en passant capture
+
+
+        #region Double move for first turn
+
+        [TestCase(4, 4, 6, 4)]
+        [TestCase(2, 1, 4, 1)]
+        [Test]
+        public void WhitePawnCanMoveTwoVerticalSpacesUpTheBoardIfItHasNotMovedBefore(int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            IPawn pawn = new WhitePawn();
+            pawn.HasMoved = false;
+            ISquare fromSquare = MockSquareWithPiece(rowFrom, colFrom, pawn);
+            ISquare toSquare = MockSquareWithoutPiece(rowTo, colTo);
+
+            bool canPawnMove = pawn.CanMove(fromSquare, toSquare);
+
+            Assert.True(canPawnMove);
+        }
+
+        [TestCase(4, 4, 6, 4)]
+        [TestCase(2, 1, 4, 1)]
+        [Test]
+        public void WhitePawnCannotMoveTwoVerticalSpacesUpTheBoardIfItHasMovedBefore(int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            IPawn pawn = new WhitePawn();
+            pawn.HasMoved = true;
+            ISquare fromSquare = MockSquareWithPiece(rowFrom, colFrom, pawn);
+            ISquare toSquare = MockSquareWithoutPiece(rowTo, colTo);
+
+            bool canPawnMove = pawn.CanMove(fromSquare, toSquare);
+
+            Assert.False(canPawnMove);
+        }
+
+        [TestCase(4, 4, 6, 4)]
+        [TestCase(2, 1, 4, 1)]
+        [Test]
+        public void WhitePawnCannotMoveTwoVerticalSpacesUpTheBoardIfItHasNotMovedButToSquareContainsAPiece(int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            IPawn pawn = new WhitePawn();
+            pawn.HasMoved = false;
+            ISquare fromSquare = MockSquareWithPiece(rowFrom, colFrom, pawn);
+            ISquare toSquare = MockSquareWithPiece(rowTo, colTo);
+
+            bool canPawnMove = pawn.CanMove(fromSquare, toSquare);
+
+            Assert.False(canPawnMove);
+        }
+
+        #endregion Double move for first turn
+
+
+        #region Other invalid moves
+
+        [TestCase(5, 2, 5, 3)]
+        [TestCase(3, 3, 3, 2)]
+        [Test]
+        public void WhitePawnCannotMoveOneHorizontalPositionAcrossTheBoard(int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            IPiece pawn = new WhitePawn();
+            ISquare fromSquare = MockSquareWithPiece(rowFrom, colFrom, pawn);
+            ISquare toSquare = MockSquareWithoutPiece(rowTo, colTo);
+
+            bool canPawnMove = pawn.CanMove(fromSquare, toSquare);
+
+            Assert.False(canPawnMove);
+        }
+
+        [TestCase(1, 1, 0, 1)]
+        [Test]
+        public void WhitePawnCannotMoveOneVerticalPositionDownTheBoard(int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            IPiece pawn = new WhitePawn();
+            ISquare fromSquare = MockSquareWithPiece(rowFrom, colFrom, pawn);
+            ISquare toSquare = MockSquareWithoutPiece(rowTo, colTo);
+
+            bool canPawnMove = pawn.CanMove(fromSquare, toSquare);
+
+            Assert.False(canPawnMove);
+        }
+
+        #endregion Other invalid moves
+    }
+}
