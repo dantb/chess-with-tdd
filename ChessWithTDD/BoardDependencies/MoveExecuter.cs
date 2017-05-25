e296c810057b19d2ba37d7cb3cfba2b159641ad2
Daniel Tattan-Birch <dantb@hotmail.co.uk>
2017-05-25 22:39:20 +0100

    Add move executer. Board currently breaks SRP, it shouldn't know how to apply the move since it's moderately complex. Let another dependency do it.

---
+++ ChessWithTDD/BoardDependencies/MoveExecuter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChessWithTDD
+{
+    public class MoveExecuter : IMoveExecuter
+    {
+        public void ExecuteMove(IBoard board, ISquare fromSquare, ISquare toSquare)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ChessWithTDD/Interfaces/BoardDependencies/IMoveExecuter.cs
@@ -0,0 +1,7 @@
+﻿namespace ChessWithTDD
+{
+    public interface IMoveExecuter
+    {
+        void ExecuteMove(IBoard board, ISquare fromSquare, ISquare toSquare);
+    }
+}
