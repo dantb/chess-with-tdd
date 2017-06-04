3267d09b559aa5aea7d5e9aff74e826e30df7ca0
Daniel Tattan-Birch <dantb@hotmail.co.uk>
2017-06-04 08:19:45 +0100

    Added first unit test for new validator

---
+++ ChessWithTDD/Tests/BoardDependencies/MoveValidation/MoveIntoCheckValidatorTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+using System.Collections.Generic;
+using static ChessWithTDD.Tests.CommonTestMethods;
+
+
+namespace ChessWithTDD.Tests
+{
+    [TestFixture]
+    public class MoveIntoCheckValidatorTests
+    {
+        [Test]
+        public void MovingWhiteKingIntoCheckReturnsTrue()
+        {
+            IBoard board = MockBoard();
+            //set up white king
+            IKing whiteKing = MockKingWithColour(Colour.White);
+            ISquare whiteKingSquare = MockSquareWithPiece(whiteKing);
+            board.Stub(b => b.WhiteKingSquare).Return(whiteKingSquare);
+            //set up enemy black piece
+            IPiece blackPiece = MockPieceWithColour(Colour.Black);
+            ISquare blackPieceSquare = MockSquareWithPiece(blackPiece);
+            board.Stub(b => b.BlackPieceSquares).Return(new HashSet<ISquare>() { blackPieceSquare });
+            //set up to square so black piece can move to it
+            ISquare toSquare = MockSquare();
+            board.Stub(b => b.MoveIsValid(blackPieceSquare, toSquare));
+            MoveIntoCheckValidator moveIntoCheckValidator = new MoveIntoCheckValidator();
+
+            bool intoCheck = moveIntoCheckValidator.MoveIsIntoCheck(board, whiteKingSquare, toSquare);
+
+            board.AssertWasCalled(b => b.MoveIsValid(blackPieceSquare, toSquare));
+            Assert.True(intoCheck);
+        }
+
+    }
+}
