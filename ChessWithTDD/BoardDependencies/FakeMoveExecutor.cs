eb6dfcc3af276879d29c39ffc20f3c7d8e8c0962
dantb <dantb@hotmail.co.uk>
2017-06-05 22:49:28 +0100

    Add fake move executor. Rethinking how this should be done - need a way to ensure the board is always reset.

---
+++ ChessWithTDD/BoardDependencies/FakeMoveExecutor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ChessWithTDD
+{
+    public class FakeMoveExecutor : IFakeMoveExecutor
+    {
+        public void ExecuteMove(IBoard board, ISquare fromSquare, ISquare toSquare)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResetBoard(IBoard board)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ChessWithTDD/Interfaces/IFakeMoveExecutor.cs
@@ -0,0 +1,8 @@
+﻿namespace ChessWithTDD
+{
+    public interface IFakeMoveExecutor
+    {
+        void ExecuteMove(IBoard board, ISquare fromSquare, ISquare toSquare);
+        void ResetBoard(IBoard board);
+    }
+}
