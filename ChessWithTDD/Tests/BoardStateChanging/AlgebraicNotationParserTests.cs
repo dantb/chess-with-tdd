429c4721412009c914d3df0f6708ad71fc4c114d
dantb <dantb@hotmail.co.uk>
2017-06-29 23:07:42 +0100

    More test cases added and null test added

--- ChessWithTDD/Tests/BoardStateChanging/AlgebraicNotationParserTests.cs
+++ ChessWithTDD/Tests/BoardStateChanging/AlgebraicNotationParserTests.cs
@@ -19,6 +19,16 @@ namespace ChessWithTDD.Tests
             Assert.AreEqual(expectedMove, move);
         }
 
+        [Test, TestCaseSource("PawnInvalidMoveParseTestCases")]
+        public void PawnMoveParseFails(string input)
+        {
+            AlgebraicNotationParser parser = new AlgebraicNotationParser();
+
+            IMove move = parser.Parse(input);
+
+            Assert.AreEqual(null, move);
+        }
+
         static object[] PawnMoveParseTestCases =
         {
             //double move up
@@ -60,6 +70,35 @@ namespace ChessWithTDD.Tests
             new object[] { "f7-f6", new Move(6, 5, 5, 5) },
             new object[] { "g7-g6", new Move(6, 6, 5, 6) },
             new object[] { "h7-h6", new Move(6, 7, 5, 7) },
+
+            //capture up
+            new object[] { "a2xb3", new Move(1, 0, 2, 1) },
+            new object[] { "b2xc3", new Move(1, 1, 2, 2) },
+            new object[] { "c2xd3", new Move(1, 2, 2, 3) },
+            new object[] { "d2xe3", new Move(1, 3, 2, 4) },
+            new object[] { "e2xf3", new Move(1, 4, 2, 5) },
+            new object[] { "f2xg3", new Move(1, 5, 2, 6) },
+            new object[] { "g2xh3", new Move(1, 6, 2, 7) },
+            new object[] { "h2xg3", new Move(1, 7, 2, 6) },
+        };
+
+        static object[] PawnInvalidMoveParseTestCases =
+        {
+            //bad connector
+            new object[] { "a2ga4" },
+            new object[] { "b25b4" },
+            new object[] { "c26c4" },
+            new object[] { "d2ld4" },
+            new object[] { "e2;e4" },
+            new object[] { "f2+f4" },
+            new object[] { "g2--g4" },
+            new object[] { "h2=h4" },
+
+            //bad cells
+            new object[] { "z2-a4" },
+            new object[] { "b66-b4" },
+            new object[] { "c2-c9" },
+            new object[] { "e0-e4" }
         };
     }
 }
