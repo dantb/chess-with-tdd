766b141dbe9b0ce700cacc59555156ab4314989b
dantb <dantb@hotmail.co.uk>
2017-07-25 22:34:53 +0100

    Separated invalid data test cases out into separate tests and parameters. This was to investigate an issue where not all my test cases are being run (or rather being seen by test explorer.) This is still the case.

--- ChessWithTDD/Tests/BoardStateChanging/AlgebraicNotationGeneratorTests.cs
+++ ChessWithTDD/Tests/BoardStateChanging/AlgebraicNotationGeneratorTests.cs
@@ -24,8 +24,35 @@ namespace ChessWithTDD.Tests
             Assert.AreEqual(expectedString, output);
         }
 
+        [Test, TestCaseSource(typeof(AlgebraicNotationGeneratorTestParameters), "PawnMoveAndCaptureGenerationInvalidMoveTestCases")]
+        public void PawnMoveTestInvalidMove(string expectedString, Move move, bool capture)
+        { 
+            AlgebraicNotationGenerator generator = new AlgebraicNotationGenerator();
+            Pawn pawn = new BlackPawn();
+            bool check = false;
+            bool checkMate = false;
+            MoveGenerationData data = new MoveGenerationData(move, check, checkMate, pawn, capture);
+
+            string output = generator.Convert(data);
+
+            Assert.AreEqual(expectedString, output);
+        }
+
         [Test, TestCaseSource(typeof(AlgebraicNotationGeneratorTestParameters), "PawnGenerationCheckAndMateTestCases")]
         public void PawnMoveTestCheckAndMateForConstantCaptureAsFalse(string expectedString, Move move, bool check, bool checkMate)
+        {
+            AlgebraicNotationGenerator generator = new AlgebraicNotationGenerator();
+            Pawn pawn = new WhitePawn();
+            bool capture = false;
+            MoveGenerationData data = new MoveGenerationData(move, check, checkMate, pawn, capture);
+
+            string output = generator.Convert(data);
+
+            Assert.AreEqual(expectedString, output);
+        }
+
+        [Test, TestCaseSource(typeof(AlgebraicNotationGeneratorTestParameters), "PawnGenerationCheckAndMateInvalidTestCases")]
+        public void PawnMoveTestInvalidCheckAndMate(string expectedString, Move move, bool check, bool checkMate)
         {
             AlgebraicNotationGenerator generator = new AlgebraicNotationGenerator();
             Pawn pawn = new WhitePawn();
