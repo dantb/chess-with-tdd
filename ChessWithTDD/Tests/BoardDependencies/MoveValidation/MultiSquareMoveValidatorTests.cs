c9df765be9a23b886ed38bccd2e50dffa071c88e
dantb <dantb@hotmail.co.uk>
2017-08-01 00:04:02 +0100

    Test cases added for multi square move dealing with adjacent sqaures

--- ChessWithTDD/Tests/BoardDependencies/MoveValidation/MultiSquareMoveValidatorTests.cs
+++ ChessWithTDD/Tests/BoardDependencies/MoveValidation/MultiSquareMoveValidatorTests.cs
@@ -166,6 +166,23 @@ namespace ChessWithTDD.Tests
             Assert.True(isBlocked);
         }
 
+        [TestCase(5, 2, 5, 3)]
+        [TestCase(5, 2, 4, 2)]
+        [Test]
+        public void MoveAdjacentReturnsTrue(int rowFrom, int colFrom, int rowTo, int colTo)
+        {
+            IPiece piece = MockPiece();
+            IPiece obstructionPiece = MockPiece();
+            ISquare fromSquare = MockSquareWithPiece(rowFrom, colFrom, piece);
+            ISquare toSquare = MockSquareWithoutPiece(rowTo, colTo);
+            IBoard board = MockBoardWithPieceInSquare(rowTo + 1, colTo - 1, obstructionPiece);
+
+            MultiSquareMoveValidator multiMoveValidator = new MultiSquareMoveValidator();
+            bool isBlocked = multiMoveValidator.MultiSquareMoveIsBlockedByAnObstacle(fromSquare, toSquare, board);
+
+            Assert.False(isBlocked);
+        }
+
         [TestCase(2, 5, 5, 2)]
         [TestCase(2, 3, 5, 6)]
         [TestCase(5, 5, 2, 2)]
