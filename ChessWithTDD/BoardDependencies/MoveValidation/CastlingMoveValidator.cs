b59cce1abac4e89f33e1c369f1e4fc7bb8405e68
dantb <dantb@hotmail.co.uk>
2017-08-02 22:49:38 +0100

    Test case templates written out but tests not created

--- ChessWithTDD/BoardDependencies/MoveValidation/CastlingMoveValidator.cs
+++ ChessWithTDD/BoardDependencies/MoveValidation/CastlingMoveValidator.cs
@@ -4,6 +4,13 @@ namespace ChessWithTDD
 {
     public class CastlingMoveValidator : ICastlingMoveValidator
     {
+        private IMoveIntoCheckValidator _moveIntoCheckValidator;
+
+        public CastlingMoveValidator(IMoveIntoCheckValidator moveIntoCheckValidator)
+        {
+            _moveIntoCheckValidator = moveIntoCheckValidator;
+        }
+
         public bool IsValidCastlingMove(IKing king, IBoard board, ISquare fromSquare, ISquare toSquare)
         {
             throw new NotImplementedException();
---
+++ ChessWithTDD/Tests/BoardDependencies/MoveValidation/CastlingMoveValidatorTests.cs
@@ -0,0 +1,80 @@
+﻿using NUnit.Framework;
+
+namespace ChessWithTDD.Tests
+{
+    [TestFixture]
+    public class CastlingMoveValidatorTests
+    {
+        [Test]
+        public void InvalidCastleIfKingHasMoved()
+        {
+
+        }
+
+        [Test]
+        public void InvalidCastleIfKingInCheck()
+        {
+
+        }
+
+        [Test]
+        public void InvalidKingSideCastleIfRightRookHasMoved()
+        {
+
+        }
+
+        [Test]
+        public void InvalidQueenSideCastleIfLeftRookHasMoved()
+        {
+
+        }
+
+        [Test]
+        public void InvalidKingSideCastleIfPieceBetweenKingAndRook()
+        {
+
+        }
+
+        [Test]
+        public void InvalidQueenSideCastleIfPieceBetweenKingAndRook()
+        {
+
+        }
+
+        [Test]
+        public void InvalidKingSideCastleIfKingMovesThroughAttackedSquare()
+        {
+
+        }
+
+        [Test]
+        public void InvalidQueenSideCastleIfKingMovesThroughAttackedSquare()
+        {
+
+        }
+
+        [Test]
+        public void InvalidKingSideCastleIfKingWouldBeInCheckAfterMove()
+        {
+
+        }
+
+        [Test]
+        public void InvalidQueenSideCastleIfKingWouldBeInCheckAfterMove()
+        {
+
+        }
+
+        [Test]
+        public void ValidKingSideCastle()
+        {
+
+        }
+
+        [Test]
+        public void ValidQueenSideCastle()
+        {
+
+        }
+    }
+}
