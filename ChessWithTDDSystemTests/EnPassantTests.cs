83edf46b620ae7137703531b6221259eaf266296
dantb <dantb@hotmail.co.uk>
2017-08-05 16:15:05 +0100

    First full test implemented - checks that en passant works on a black pawn at the system level.

--- ChessWithTDDSystemTests/EnPassantTests.cs
+++ ChessWithTDDSystemTests/EnPassantTests.cs
@@ -1,33 +1,37 @@
 ﻿using Autofac;
 using ChessWithTDD;
 using NUnit.Framework;
+using System;
+using System.IO;
 
 namespace ChessWithTDDSystemTests
 {
     [TestFixture]
     public class EnPassantTests
     {
-        private const string EnPassantFolderPath = "../PositionFiles/EnPassant/";
-        private const string WhitePawnCapturesBlackPawnUsingEnPassantFile = "WhitePawnCapturesBlackPawnUsingEnPassant";
+        private const string EnPassantFolder = "EnPassant";
+        private const string WhitePawnCapturesBlackPawnUsingEnPassantFile = "WhitePawnCapturesBlackPawnUsingEnPassant.txt";
 
+        private string _positionFilesFolder;
         private PositionLoader _positionLoader;
 
         [OneTimeSetUp]
         public void SetUp()
         {
             _positionLoader = new PositionLoader();
+            _positionFilesFolder =  Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"../../PositionFiles/"));
         }
 
         [Test]
         public void WhitePawnCapturesBlackPawnUsingEnPassant()
         {
-            const string path =  EnPassantFolderPath + WhitePawnCapturesBlackPawnUsingEnPassantFile;
+            string path = Path.Combine(_positionFilesFolder, EnPassantFolder, WhitePawnCapturesBlackPawnUsingEnPassantFile);
             IBoard board = NewBoard();
 
             _positionLoader.LoadPositionIntoBoard(board, path);
 
-            //Assert.That();
-            Assert.Fail();
+            ISquare takenBlackPawnSquare = board.GetSquare(4, 5);
+            Assert.False(takenBlackPawnSquare.ContainsPiece);
         }
 
         private IBoard NewBoard()
