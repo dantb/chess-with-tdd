d8e4ccafe0101b5832b173ad22bd12270f1420e0
dantb <dantb@hotmail.co.uk>
2017-08-05 15:22:23 +0100

    New project for system level tests for the underlying board

---
+++ ChessWithTDDSystemTests/EnPassantTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace ChessWithTDDSystemTests
+{
+    [TestFixture]
+    public class EnPassantTests
+    {
+        private const string EnPassantFolderPath = "../PositionFiles/EnPassant/";
+
+        [Test]
+        public void PawnCapturedByEnPassantCorrectly()
+        {
+            Assert.Fail();
+        }
+    }
+}
