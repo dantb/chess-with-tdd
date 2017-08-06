a0d7ad95e3a7644e31255172b45f767bae856d3a
dantb <dantb@hotmail.co.uk>
2017-08-06 01:32:21 +0100

    Castling executor added, tests and implementation done.

---
+++ ChessWithTDD/BoardDependencies/CastlingExecutor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using static ChessWithTDD.BoardConstants;
+
+namespace ChessWithTDD
+{
+    /// <summary>
+    /// The responsibility of this class is to execute the rook's movement part of the castling move, as well as making sure
+    /// the rook and king have there HasMoved set.
+    /// </summary>
+    public class CastlingExecutor : ICastlingExecutor
+    {
+        /// <summary>
+        /// Executes special requirements necessary for a castling move. These are:
+        /// 
+        /// 1) The rook must move to the other side of the king.
+        /// 2) The rook HasMoved is set to true.
+        /// 3) The king HasMoved is set to true.
+        /// 
+        /// This method assumes there is a king in the fromSquare about to move
+        /// </summary>
+        public void ExecuteCastlingMove(ISquare fromSquare, ISquare toSquare, IBoard board)
+        {
+            bool sameRowMove = fromSquare.Row - toSquare.Row == 0;
+            bool twoColsMove = Math.Abs(fromSquare.Col - toSquare.Col) == 2;
+            if (sameRowMove && twoColsMove)
+            {
+                bool queenside = fromSquare.Col - toSquare.Col == 2;
+
+                ISquare rookSquare = queenside
+                   ? board.GetSquare(fromSquare.Row, LEFT_ROOK_COL)   //queenside castle
+                   : board.GetSquare(fromSquare.Row, RIGHT_ROOK_COL); //kingside castle
+
+                ISquare rookToSquare = queenside
+                   ? board.GetSquare(fromSquare.Row, LEFT_ROOK_COL + 3)
+                   : board.GetSquare(fromSquare.Row, RIGHT_ROOK_COL - 2);
+
+                (rookSquare.Piece as IRook).HasMoved = true;
+
+                rookToSquare.ContainsPiece = true;
+                rookToSquare.Piece = rookSquare.Piece;
+                rookSquare.ContainsPiece = false;
+                rookSquare.Piece = null;
+                board.PendingUpdates.Add(rookToSquare);
+                board.PendingUpdates.Add(rookSquare);
+            }
+
+            // this should be set regardless of whether this is a castling move or not
+            (fromSquare.Piece as IKing).HasMoved = true;
+        }
+    }
+}
---
+++ ChessWithTDD/Tests/BoardDependencies/CastlingExecutorTests.cs
@@ -0,0 +1,100 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+using System.Collections.Generic;
+using static ChessWithTDD.BoardConstants;
+using static ChessWithTDD.Tests.TestHelpers.CommonTestMethods;
+
+namespace ChessWithTDD.Tests
+{
+    [TestFixture]
+    public class CastlingExecutorTests
+    {
+        [TestCase(WHITE_BACK_ROW, KING_COLUMN, WHITE_BACK_ROW, KING_COLUMN + 1, WHITE_BACK_ROW, RIGHT_ROOK_COL, WHITE_BACK_ROW, KING_COLUMN - 1)]
+        [TestCase(WHITE_BACK_ROW, KING_COLUMN, WHITE_BACK_ROW, KING_COLUMN - 1, WHITE_BACK_ROW, RIGHT_ROOK_COL, WHITE_BACK_ROW, KING_COLUMN + 1)]
+        [TestCase(WHITE_BACK_ROW, KING_COLUMN, WHITE_BACK_ROW + 1, KING_COLUMN, WHITE_BACK_ROW, RIGHT_ROOK_COL, WHITE_BACK_ROW, KING_COLUMN + 1)]
+        [TestCase(WHITE_BACK_ROW, KING_COLUMN, WHITE_BACK_ROW + 1, KING_COLUMN + 1, WHITE_BACK_ROW, RIGHT_ROOK_COL, WHITE_BACK_ROW, KING_COLUMN - 1)]
+        [TestCase(WHITE_BACK_ROW, KING_COLUMN, WHITE_BACK_ROW + 1, KING_COLUMN - 1, WHITE_BACK_ROW, RIGHT_ROOK_COL, WHITE_BACK_ROW, KING_COLUMN + 1)]
+        [TestCase(BLACK_BACK_ROW, KING_COLUMN, BLACK_BACK_ROW, KING_COLUMN + 1, BLACK_BACK_ROW, RIGHT_ROOK_COL, BLACK_BACK_ROW, KING_COLUMN - 1)]
+        [TestCase(BLACK_BACK_ROW, KING_COLUMN, BLACK_BACK_ROW, KING_COLUMN - 1, BLACK_BACK_ROW, LEFT_ROOK_COL, BLACK_BACK_ROW, KING_COLUMN + 1)]
+        [TestCase(BLACK_BACK_ROW, KING_COLUMN, BLACK_BACK_ROW - 1, KING_COLUMN, BLACK_BACK_ROW, RIGHT_ROOK_COL, BLACK_BACK_ROW, KING_COLUMN + 1)]
+        [TestCase(BLACK_BACK_ROW, KING_COLUMN, BLACK_BACK_ROW - 1, KING_COLUMN + 1, BLACK_BACK_ROW, RIGHT_ROOK_COL, BLACK_BACK_ROW, KING_COLUMN - 1)]
+        [TestCase(BLACK_BACK_ROW, KING_COLUMN, BLACK_BACK_ROW - 1, KING_COLUMN - 1, BLACK_BACK_ROW, LEFT_ROOK_COL, BLACK_BACK_ROW, KING_COLUMN + 1)]
+        [Test]
+        public void KingMoveToAdjacentSquareDoesNothingExceptSetKingHasMovedTrue(int kingRow, int kingCol, int toRow, int toCol,
+            int rookRow, int rookCol, int rookFinalRow, int rookFinalCol)
+        {
+            CastlingExecutor executor = new CastlingExecutor();
+            ISquare toSquare = MockSquare(toRow, toCol);
+            IKing king = MockKingWithHasMoved(false);
+            ISquare fromSquare = MockSquareWithPiece(kingRow, kingCol, king);
+            IRook rook = MockRookWithHasMoved(false);
+            ISquare rookSquare = MockSquareWithPiece(rookRow, rookCol, rook);
+            ISquare rookFinalSquare = MockSquare(rookFinalRow, rookFinalCol);
+            IBoard board = MockBoardWithGetSquaresMocked();
+            List<ISquare> pendingUpdates = new List<ISquare>();
+            board.Stub(b => b.PendingUpdates).Return(pendingUpdates);
+            StubSquareOnBoard(board, fromSquare);
+            StubSquareOnBoard(board, toSquare);
+            StubSquareOnBoard(board, rookSquare);
+            StubSquareOnBoard(board, rookFinalSquare);
+
+            executor.ExecuteCastlingMove(fromSquare, toSquare, board);
+
+            // assert not moved
+            rookFinalSquare.AssertWasNotCalled(r => r.ContainsPiece = true);
+            rookFinalSquare.AssertWasNotCalled(r => r.Piece = rook);
+            rookSquare.AssertWasNotCalled(r => r.ContainsPiece = false);
+            rookSquare.AssertWasNotCalled(r => r.Piece = null);
+
+            // assert that HasMoved states not changed
+            rook.AssertWasNotCalled(r => r.HasMoved = true);
+
+            // important - king HasMoved should have changed
+            king.AssertWasCalled(k => k.HasMoved = true);
+
+            // assert rook from and rook final squares not added to pending updates
+            Assert.False(pendingUpdates.Contains(rookSquare));
+            Assert.False(pendingUpdates.Contains(rookFinalSquare));
+        }
+
+        [TestCase(WHITE_BACK_ROW, KING_COLUMN, WHITE_BACK_ROW, KING_COLUMN + 2, WHITE_BACK_ROW, RIGHT_ROOK_COL, WHITE_BACK_ROW, KING_COLUMN + 1)] //white kingside
+        [TestCase(WHITE_BACK_ROW, KING_COLUMN, WHITE_BACK_ROW, KING_COLUMN - 2, WHITE_BACK_ROW, LEFT_ROOK_COL, WHITE_BACK_ROW, KING_COLUMN - 1)]  //white queenside
+        [TestCase(BLACK_BACK_ROW, KING_COLUMN, BLACK_BACK_ROW, KING_COLUMN + 2, BLACK_BACK_ROW, RIGHT_ROOK_COL, BLACK_BACK_ROW, KING_COLUMN + 1)] //black kingside
+        [TestCase(BLACK_BACK_ROW, KING_COLUMN, BLACK_BACK_ROW, KING_COLUMN - 2, BLACK_BACK_ROW, LEFT_ROOK_COL, BLACK_BACK_ROW, KING_COLUMN - 1)]  //black queenside
+        [Test]
+        public void KingMoveTwoSquaresMovesRookAndSetsHasMovedStatesToTrue(int kingRow, int kingCol, int toRow, int toCol, 
+            int rookRow, int rookCol, int rookFinalRow, int rookFinalCol)
+        {
+            CastlingExecutor executor = new CastlingExecutor();
+            ISquare toSquare = MockSquare(toRow, toCol);
+            IKing king = MockKingWithHasMoved(false);
+            ISquare fromSquare = MockSquareWithPiece(kingRow, kingCol, king);
+            IRook rook = MockRookWithHasMoved(false);
+            ISquare rookSquare = MockSquareWithPiece(rookRow, rookCol, rook);
+            ISquare rookFinalSquare = MockSquare(rookFinalRow, rookFinalCol);
+            IBoard board = MockBoardWithGetSquaresMocked();
+            List<ISquare> pendingUpdates = new List<ISquare>();
+            board.Stub(b => b.PendingUpdates).Return(pendingUpdates);
+            StubSquareOnBoard(board, fromSquare);
+            StubSquareOnBoard(board, toSquare);
+            StubSquareOnBoard(board, rookSquare);
+            StubSquareOnBoard(board, rookFinalSquare);
+
+            executor.ExecuteCastlingMove(fromSquare, toSquare, board);
+
+            // assert rook was moved correctly
+            rookFinalSquare.AssertWasCalled(r => r.ContainsPiece = true);
+            rookFinalSquare.AssertWasCalled(r => r.Piece = rook);
+            rookSquare.AssertWasCalled(r => r.ContainsPiece = false);
+            rookSquare.AssertWasCalled(r => r.Piece = null);
+
+            // assert that HasMoved states were change correctly
+            king.AssertWasCalled(k => k.HasMoved = true);
+            rook.AssertWasCalled(r => r.HasMoved = true);
+
+            // assert rook from and rook final squares added to pending updates
+            Assert.True(pendingUpdates.Contains(rookSquare));
+            Assert.True(pendingUpdates.Contains(rookFinalSquare));
+        }
+    }
+}
