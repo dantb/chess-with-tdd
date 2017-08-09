289d2a76eb24d8eb6b2a1f4f18c6295ea42a4b87
dantb <dantb@hotmail.co.uk>
2017-08-09 20:06:01 +0100

    Slight engine performance increase - it now values the board state and will prefer to take pieces where possible.

--- ChessEngine/Engine.cs
+++ ChessEngine/Engine.cs
@@ -1,16 +1,30 @@
-﻿using ChessWithTDD;
-using System;
-using System.Threading;
+﻿using System;
+using System.Collections.Generic;
+using ChessWithTDD;
+using Autofac;
 
 namespace ChessEngine
 {
     public class Engine : IMoveProvider
     {
+        private static Dictionary<Type, double> PieceTypeToPoints = new Dictionary<Type, double>()
+        {
+            { typeof(WhitePawn), 10 },
+            { typeof(BlackPawn), 10 },
+            { typeof(Knight), 30 },
+            { typeof(Bishop), 30 },
+            { typeof(Rook), 50 },
+            { typeof(Queen), 90 },
+            { typeof(King), 900 }
+        };
+
         public event MoveProviderEventHandler MoveChosenEvent;
 
         public Move CalculateBestMove(IBoard board)
         {
-            foreach (ISquare ourTeamSquare  in board.MovingTeamPieceSquares)
+            List<SquaresForMoves> validMoves = new List<SquaresForMoves>();
+
+            foreach (ISquare ourTeamSquare in board.MovingTeamPieceSquares)
             {
                 foreach (var row in board.Squares)
                 {
@@ -18,14 +32,79 @@ namespace ChessEngine
                     {
                         if (board.MoveIsValid(ourTeamSquare, square))
                         {
-                            Thread.Sleep(5000);
-                            return new Move(ourTeamSquare.Row, ourTeamSquare.Col, square.Row, square.Col);
+                            validMoves.Add(new SquaresForMoves(ourTeamSquare, square));
                         }
                     }
                 }
             }
-            return new Move(0, 0, 0, 0);
-            throw new NotImplementedException();
+
+            double bestMovePoints = -9999999;
+            SquaresForMoves bestMove = null;
+
+            foreach (SquaresForMoves validMove in validMoves)
+            {
+                IBoard newBoard = GetNewBoardWithThisMoveApplied(board, validMove.FromSquare, validMove.ToSquare);
+                // make this negative since the team whose turn it is is now the other team
+                double boardValue = -1 * EvaluateBoard(newBoard, PieceTypeToPoints);
+                if (bestMovePoints < boardValue)
+                {
+                    bestMovePoints = boardValue;
+                    bestMove = validMove;
+                }
+            }
+
+            return new Move(bestMove.FromSquare.Row, bestMove.FromSquare.Col, bestMove.ToSquare.Row, bestMove.ToSquare.Col);
+        }
+
+        /// <summary>
+        /// Evaluate the "value" of the board for the team currently moving. High positive value indicates a better
+        /// position and a high negative value indicates a worse position for the moving team.
+        /// </summary>
+        private double EvaluateBoard(IBoard board, Dictionary<Type, double> piecePointEvaluations)
+        {
+            double result = 0;
+
+            foreach (ISquare movingTeamPieceSquare in board.MovingTeamPieceSquares)
+            {
+                result += piecePointEvaluations[movingTeamPieceSquare.Piece.GetType()];
+            }
+
+            foreach (ISquare otherTeamPieceSquare in board.OtherTeamPieceSquares)
+            {
+                result -= piecePointEvaluations[otherTeamPieceSquare.Piece.GetType()];
+            }
+
+            return result;
+        }
+
+        private IBoard GetNewBoardWithThisMoveApplied(IBoard theBoard, ISquare fromSquare, ISquare toSquare)
+        {
+            IBoard newBoard;
+            using (var scope = ContainerConfiguration.Container.BeginLifetimeScope())
+            {
+                newBoard = scope.Resolve<IBoard>();
+            }
+            foreach (MoveGenerationData data in theBoard.OrderedMoveData)
+            {
+                ISquare fs = newBoard.GetSquare(data.Move.FromRow, data.Move.FromCol);
+                ISquare ts = newBoard.GetSquare(data.Move.ToRow, data.Move.ToCol);
+                newBoard.Apply(fs, ts);
+            }
+            ISquare newBoardFromSquare = newBoard.GetSquare(fromSquare.Row, fromSquare.Col);
+            ISquare newBoardToSquare = newBoard.GetSquare(toSquare.Row, toSquare.Col);
+            newBoard.Apply(newBoardFromSquare, newBoardToSquare);
+            return newBoard;
+        }
+
+        public class SquaresForMoves
+        {
+            public ISquare FromSquare { get; }
+            public ISquare ToSquare { get; }
+            public SquaresForMoves(ISquare fromSquare, ISquare toSquare)
+            {
+                FromSquare = fromSquare;
+                ToSquare = toSquare;
+            }
         }
     }
 }
