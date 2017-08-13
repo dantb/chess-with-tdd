5d3a480886d82a27a646d7169913d8eec9d43414
dantb <dantb@hotmail.co.uk>
2017-08-13 22:18:48 +0100

    Added deep copy class - taken from https://www.codeproject.com/Articles/1111658/Fast-Deep-Copy-by-Expression-Trees-C-Sharp
    This appears to do everything I need for cloning, without the overhead of serialisation / reflection. Will write tests to check whether it actually works.

---
+++ ChessWithTDDSystemTests/DeepCopyByExpressionTreesTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace ChessWithTDDSystemTests
+{
+    /// <summary>
+    /// Performs various system tests and clones the board to ensure that the deep copy is working correctly
+    /// </summary>
+    [TestFixture]
+    public class DeepCopyByExpressionTreesTests
+    {
+        [Test]
+        public void DeepCopyCopiesBoardCorrectly()
+        {
+
+        }
+
+    }
+}
